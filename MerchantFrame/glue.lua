f98670b1c2dd5067a3747ce3ee51a775fa28532c
kaelonR <J.dekoning141@gmail.com>
2024-09-12 16:26:56 +0200

    Introduce compatibility layer, add SellJunk and ArkInventory compatibility

--- MerchantFrame/glue.lua
+++ MerchantFrame/glue.lua
@@ -27,3 +27,57 @@ end )
 hooksecurefunc('CloseMerchant', function() 
   BuyEmAllFrame:Hide();
 end)
+
+local compatManager = {
+  eventFrame = CreateFrame('Frame');
+  entry = function(compatFunc) 
+    local entry = { loaded = false };
+    entry.onLoaded = function()
+      if(entry.loaded) then return end
+      compatFunc();
+      entry.loaded = true;
+    end
+    return entry;
+  end,
+  init = function(self)
+    for key, entry in pairs(self) do
+      print(key);
+      if(key ~= 'entry' and key ~= 'init' and type(entry) == 'table' and type(entry.onLoaded) == 'function') then
+        if(select(2, C_AddOns.IsAddOnLoaded(key))) then
+          entry.onLoaded()
+        end
+      end
+    end
+  end
+}
+compatManager.eventFrame:RegisterEvent('ADDON_LOADED');
+compatManager.eventFrame:SetScript('OnEvent', function(self, event, arg1)
+  if(event ~= 'ADDON_LOADED') then return end
+  
+  local addonName = arg1;
+  if(compatManager[addonName] ~= nil and type(compatManager[addonName].onLoaded) == 'function') then
+    compatManager[addonName].onLoaded();
+  end
+end)
+
+compatManager.SellJunk = compatManager.entry(function()
+  local sellButton = LibStub("AceAddon-3.0"):GetAddon("SellJunk").sellButton;
+  sellButton:SetParent(BuyEmAllFrame);
+end)
+
+compatManager.ArkInventory = compatManager.entry(function()
+  local originalHook = ArkInventory.HookOpenAllBags;
+
+  ArkInventory.HookOpenAllBags = function(self, ...)
+    local who = ...
+    local whoName = who
+    if who then
+      whoName = who:GetName( )
+    end
+    if(whoName == 'BuyEmAllFrame') then return end;
+  
+    originalHook(self, ...);
+  end
+end)
+
+compatManager:init();
